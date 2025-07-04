cbd797fa2f92c10562a5b3189cebaadf4a943295
Al Amin Khan <alamin24.7khan@outlook.com>
2025-07-04 10:04:57 -0700

    update

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Marquee from "react-fast-marquee";
 import { Link } from 'react-router-dom';
 
 export default function LatestNews() {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    fetch('https://openapi.programming-hero.com/api/news/category/01')
+      .then(res => res.json())
+      .then(data => setNews(data.data.slice(0, 4)));
+  }, []);
+
   return (
     <div className='flex gap-2 items-center p-2 bg-gray-200'>
       <p className='bg-[#D72050] text-white px-4 py-2 rounded-[2px]'>Latest</p>
       <Marquee pauseOnHover={true} className='space-x-10'>
-        <Link to="/news">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum, ducimus!</Link>
-        <Link to="/news">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum, ducimus!</Link>
-        <Link to="/news">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum, ducimus!</Link>
-        <Link to="/news">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum, ducimus!</Link>
+        {news.map((item, index) => (
+          <Link key={index} to={`/news/${item._id}`} className='mr-5 font-semibold text-gray-800 hover:text-blue-600 transition-colors'>
+            {item.title}
+          </Link>
+        ))}
       </Marquee>
-      <p></p>      
     </div>
   )
 }
--- src/components/Layout_components/LeftNavbar.jsx
+++ src/components/Layout_components/LeftNavbar.jsx
@@ -15,7 +15,11 @@ export default function LeftNavbar() {
       <h2 className='font-bold'>All Category ({categories.length})</h2>
       <div className='flex flex-col gap-2'>
         {categories.map((category) => (
-          <NavLink to={`/category/${category.category_id}`} className='bg-gray-50 hover:bg-gray-300 p-2 rounded' key={category.category_id}>
+          <NavLink 
+            to={`/category/${category.category_id}`} 
+            className={({isActive}) => `p-2 rounded ${isActive ? 'bg-blue-500 text-white' : 'bg-gray-50 hover:bg-gray-300'}`} 
+            key={category.category_id}
+          >
             {category.category_name}
           </NavLink>
         ))}
